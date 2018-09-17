6d803720f239ccea5cf8ecde4f6e3df428f06616
Victory_Jukyung Kim <victoryjkkim92@gmail.com>
2018-09-17 18:51:53 +0900

    Update Web Cafe 1-6.js
    jquery 주석

--- HTML-CSS/Exercise/Web Cafe 1-6.js
+++ HTML-CSS/Exercise/Web Cafe 1-6.js
@@ -15,6 +15,10 @@ item.on('mouseover focusin', function (){
     $(this).addClass('menu-act');
 });
 
+//이런 이벤트가 활성화되었을 때 먼저 일어남
+// 두 가지 이벤트가 선택되었을 때, menu-act클래스를 실행
+//마우스가 올라가진 부분에만 적용시키고 싶을때 $(this)
+
 subItem.hover(function (){
     $(this).toggleClass('fa-angle-right');
 });
@@ -25,14 +29,17 @@ tab.on('click focusin',function(){
     $(this).parent().addClass('tab-act');
 });
 
+// 변수를 선언해준 tab.부분에 click과 focusin이라는 두 이벤트가 일어났을 떄, 
+// 1. 부모요소 밑에 있는 자식요소들에게서 'tab-act'를 제거하고(만약에 제거하지 않으면 화면에 출력됨)
+// 2. 부모요소에게 'tab-act'라는 클래스를 실행시켜라
 
 
 
 
 
-//이런 이벤트가 활성화되었을 때 먼저 일어남
-// 두 가지 이벤트가 선택되었을 때, menu-act클래스를 실행
-//마우스가 올라가진 부분에만 적용시키고 싶을때 $(this)
+
+
+
 
 
 
