7bb739844289087178d6221c8d60ed70cf023adf
Victory_Jukyung Kim <victoryjkkim92@gmail.com>
2018-09-18 17:13:04 +0900

    Update webcafe7.js

--- HTML-CSS/Exercise/Web Cafe 1-6.js
+++ HTML-CSS/Exercise/Web Cafe 1-6.js
@@ -2,7 +2,7 @@
 var item = $('.menu-item'); // 변수 선언
 var subItem = $('.sub-menu a'); // 변수 선언 .sub-menu하위 a태그
 var tab = $('.tab'); // 변수선언 
-
+var list= $('.related-list');
 
 // 메인 메뉴 제어를 위한 함수 
 
@@ -28,6 +28,22 @@ tab.on('click focusin',function(){
     $(this).parent().siblings().removeClass('tab-act');
     $(this).parent().addClass('tab-act');
 });
+
+// 관련사이트 목록 에니메이션을 위한 함수
+
+// list.hover(function(){
+//     $(this).toggleClass('related-act');
+// });
+
+list.on('mouseover focusin', function () {
+    $(this).addClass('related-act');
+});
+
+list.on('mouseout focusout', function () {
+    $(this).removeClass('related-act');
+});
+
+
 
 // 변수로 선언해준 tab부분에 click이나 focusin이라는 이벤트가 일어났을 떄, 
 // 1. 선택된 this의(이벤트가 일어난 부분) 부모요소와 형제관계에 있는 요소를 찾아, 
